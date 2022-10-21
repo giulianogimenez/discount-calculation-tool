7d28fa5b295d952ef3261b5a57eeaf526ac9b7a3
giuliano.gimenez <ggimenez87@gmail.com>
2022-10-21 18:23:45 -0300

    Fixing repos

--- project.clj
+++ project.clj
@@ -4,11 +4,5 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :plugins [[lein-midje "3.2.2"]
-            [lein-cloverage "1.2.2"]
-            [lein-vanity "0.2.0"]
-            [s3-wagon-private "1.3.5"]
-            [lein-ancient "0.7.0-SNAPSHOT"]]
-
   :repositories [["central" {:url "https://repo1.maven.org/maven2/" :snapshots false}]
                  ["clojars" {:url "https://clojars.org/repo/"}]])
